05d4765be6ef31de48ecb5df717b78c50864473f
vasiljevp <81388791+vasiljevp@users.noreply.github.com>
2021-10-02 20:24:48 -0400

    Restructure src folder
    I cleaned up a bunch of files that create-react-app generated since we don't need them. The components folder now has some starter components in it.

---
+++ src/react-app/src/components/AppContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginRegister from './LoginRegister'
+
+class AppContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <LoginRegister />
+            </div>
+        )
+    }
+}
+
+export default AppContainer
---
+++ src/react-app/src/components/LoginRegister.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class LoginRegister extends React.Component {
+    render() {
+        return (
+            <h1>hey, this is a test</h1>
+        )
+    }
+}
+
+export default LoginRegister
