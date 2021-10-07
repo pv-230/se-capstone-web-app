816eea5076f0385fe02a33394e7e86ee8b1f55d3
vasiljevp <81388791+vasiljevp@users.noreply.github.com>
2021-10-07 13:53:12 -0400

    Restructure Home.js
    Has the same logic but slightly changed readability

--- src/react-app/src/pages/Home.js
+++ src/react-app/src/pages/Home.js
@@ -4,12 +4,15 @@ import React from 'react'
  * This is the page that houses all components needed in the home page.
  */
 const Home = (props) => {
-  if (props.uidProp !== null) {
-    return <h1 >Home Page</h1>
-  } else {
-    window.location.href = '/login'
-    return <></>
+  if (props.uidProp === null) {
+    // Attempts to visit the homepage if not logged in will redirect to the login page
+    window.location.href = '/login';
+    return null;
   }
+
+  return (
+    <h1>Home Page</h1>
+  )
 }
 
 export default Home
