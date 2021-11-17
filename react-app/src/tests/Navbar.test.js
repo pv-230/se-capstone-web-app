9caa3ca6a51145f55c6b80e91e20e15ac015f1cf
vasiljevp <81388791+vasiljevp@users.noreply.github.com>
2021-11-17 01:15:39 -0500

    Add a few more navbar tests
    I can't add too many more tests at the moment for the navbar because I
    need to figure out the right way to mock our app so I can see state
    changes

--- react-app/src/tests/Navbar.test.js
+++ react-app/src/tests/Navbar.test.js
@@ -1,7 +1,56 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import NavBar from '../components/NavBar';
 
-it('renders navbar title based on prop', () => {
-  render(<NavBar navTitle = "Test Title" />);
-  expect(screen.getByText(/test title/i)).toBeInTheDocument();
+// Navbar tests
+describe('navbar tests', () => {
+  it('sets navbar title based on prop', () => {
+    render(<NavBar navTitle="Test Title" />);
+    const navBar = screen.getByRole('banner');
+    expect(navBar).toBeInTheDocument();
+  });
+
+  it('hides navbar when title prop not set', () => {
+    render(<NavBar />);
+    const navBar = screen.queryByRole('banner');
+    expect(navBar).not.toBeInTheDocument();
+  });
+
+  // Currently this test doesn't work because I need to mock the entire app first, still learning
+  // how to do that properly
+  
+  // it('theme toggle switches between light and dark', () => {
+  //   render(<NavBar navTitle="Test Title" />);
+  //   const themeControlLabel = screen.getByTestId('themeControlLabel');
+  //   const themeSwitch = screen.getByTestId('themeSwitch');
+  //   expect(themeControlLabel).toHaveTextContent('Dark');
+  //   userEvent.click(themeSwitch);
+  //   expect(themeControlLabel).toHaveTextContent('Light');
+  // });
+});
+
+// Nav menu tests
+describe('nav menu tests', () => {
+  it('nav menu opens when clicked', () => {
+    render(<NavBar navTitle="Test Title" />);
+    const navMenuButton = screen.getByTestId('navMenuButton');
+    userEvent.click(navMenuButton);
+    const navMenu = screen.getByRole('menu');
+    expect(navMenu).toBeInTheDocument();
+  });
+
+  // TODO: Add tests to check redirections, requires mocking the app
+});
+
+// Account menu tests
+describe('account menu tests', () => {
+  it('account menu opens when clicked', () => {
+    render(<NavBar navTitle="Test Title" />);
+    const accountMenuButton = screen.getByTestId('accountMenuButton');
+    userEvent.click(accountMenuButton);
+    const accountMenu = screen.getByRole('menu');
+    expect(accountMenu).toBeInTheDocument();
+  });
+
+  // TODO: Add tests to check redirections, requires mocking the app
 });
