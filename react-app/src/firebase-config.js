537f774a930f1174da4bc4c112c46eb892488f76
Pete <81388791+pv-230@users.noreply.github.com>
2022-11-15 11:24:14 -0500

    Remove firebase API key

---
+++ react-app/src/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getDatabase } from "firebase/database";
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+let db = getFirestore();
+export { db };
